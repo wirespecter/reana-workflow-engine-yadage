b8b219ce2ba7a6886f41438b067e1299e0f7393f
Anton Khodak <anton.khodak@ukr.net>
2017-11-03 15:08:10 +0200

    config: change variable name to a generic one

--- reana_workflow_engine_yadage/externalbackend.py
+++ reana_workflow_engine_yadage/externalbackend.py
@@ -106,7 +106,7 @@ class ExternalBackend(object):
         log.info('submitting!')
 
         job_id = submit.submit(
-            os.getenv('REANA_WORKFLOW_ENGINE_YADAGE_EXPERIMENT', 'default'),
+            os.getenv('REANA_WORKFLOW_ENGINE_EXPERIMENT', 'default'),
             image, wrapped_cmd)
 
         log.info('submitted job: %s', job_id)
