43d81731736706c107bf8be68ea2e541de9cc248
Vladyslav Moisieienkov <vladyslav.moisieienkov@cern.ch>
2022-03-31 14:23:39 +0200

    installation: bumped all dependencies

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ readme = open("README.rst").read()
 history = open("CHANGES.rst").read()
 
 tests_require = [
-    "pytest-reana>=0.9.0a3,<0.10.0",
+    "pytest-reana>=0.9.0a4,<0.10.0",
 ]
 
 extras_require = {
